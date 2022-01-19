17a9c572fe7952aee56dc8612b30a2b0600126cd
Meiling Bedard <meiling.bedard@gmail.com>
2022-01-19 14:43:07 -0500

    add google analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-217561080-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 ReactDOM.render(
   <React.StrictMode>
